d147626f1572a084234222cf6bb1e481c8e4d95c
vonhyou <moi@rfung.dev>
2025-08-29 10:46:54 -0400

    Fix quaternion benchmarks and analyze matrix benchmarks

--- 02_math_libs_benchmark/src/benchmarks/matrix.zig
+++ 02_math_libs_benchmark/src/benchmarks/matrix.zig
@@ -7,18 +7,18 @@ pub fn bench_zalgebra_mat_mul(allocator: std.mem.Allocator) void {
     _ = allocator;
     const mat_a = zalgebra.Mat4.identity();
     const translation_vec = zalgebra.Vec3.new(1, 2, 3);
-    const mat_b = zalgebra.Mat4.fromTranslation(translation_vec);
-    
+    const mat_b = zalgebra.Mat4.fromTranslate(translation_vec);
+
     var result = zalgebra.Mat4.mul(mat_a, mat_b);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_mat_mul(allocator: std.mem.Allocator) void {
     _ = allocator;
-    const mat_a = zm.identity();
-    const mat_b = zm.translation(1, 2, 3);
-    
-    var result = zm.mul(mat_a, mat_b);
+    const mat_a = zm.Mat4.identity();
+    const mat_b = zm.Mat4.translation(1, 2, 3);
+
+    var result = zm.Mat4.multiply(mat_a, mat_b);
     std.mem.doNotOptimizeAway(&result);
 }
 
@@ -26,33 +26,33 @@ pub fn bench_zm_mat_mul(allocator: std.mem.Allocator) void {
 pub fn bench_zalgebra_mat_transpose(allocator: std.mem.Allocator) void {
     _ = allocator;
     const translation_vec = zalgebra.Vec3.new(1, 2, 3);
-    const mat = zalgebra.Mat4.fromTranslation(translation_vec);
-    
+    const mat = zalgebra.Mat4.fromTranslate(translation_vec);
+
     var result = zalgebra.Mat4.transpose(mat);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_mat_transpose(allocator: std.mem.Allocator) void {
     _ = allocator;
-    const mat = zm.translation(1, 2, 3);
-    
-    var result = zm.transpose(mat);
+    const mat = zm.Mat4.translation(1, 2, 3);
+
+    var result = zm.Mat4.transpose(mat);
     std.mem.doNotOptimizeAway(&result);
 }
 
-// Matrix transformation benchmarks  
+// Matrix transformation benchmarks
 pub fn bench_zalgebra_transform_matrix(allocator: std.mem.Allocator) void {
     _ = allocator;
     const translation = zalgebra.Vec3.new(1, 2, 3);
-    
-    var result = zalgebra.Mat4.fromTranslation(translation);
+
+    var result = zalgebra.Mat4.fromTranslate(translation);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_transform_matrix(allocator: std.mem.Allocator) void {
     _ = allocator;
-    
-    var result = zm.translation(1, 2, 3);
+
+    var result = zm.Mat4.translation(1, 2, 3);
     std.mem.doNotOptimizeAway(&result);
 }
 
@@ -61,21 +61,20 @@ pub fn bench_zalgebra_matrix_chain(allocator: std.mem.Allocator) void {
     _ = allocator;
     const mat1 = zalgebra.Mat4.identity();
     const translation_vec = zalgebra.Vec3.new(1, 2, 3);
-    const mat2 = zalgebra.Mat4.fromTranslation(translation_vec);
-    const mat3 = zalgebra.Mat4.fromTranslation(zalgebra.Vec3.new(4, 5, 6));
-    
-    var temp = zalgebra.Mat4.mul(mat1, mat2);
-    var result = zalgebra.Mat4.mul(temp, mat3);
+    const mat2 = zalgebra.Mat4.fromTranslate(translation_vec);
+    const mat3 = zalgebra.Mat4.fromTranslate(zalgebra.Vec3.new(4, 5, 6));
+
+    var result = zalgebra.Mat4.mul(zalgebra.Mat4.mul(mat1, mat2), mat3);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_matrix_chain(allocator: std.mem.Allocator) void {
     _ = allocator;
-    const mat1 = zm.identity();
-    const mat2 = zm.translation(1, 2, 3);
-    const mat3 = zm.translation(4, 5, 6);
-    
-    var temp = zm.mul(mat1, mat2);
-    var result = zm.mul(temp, mat3);
+    const mat1 = zm.Mat4.identity();
+    const mat2 = zm.Mat4.translation(1, 2, 3);
+    const mat3 = zm.Mat4.translation(4, 5, 6);
+
+    var result = zm.Mat4.multiply(zm.Mat4.multiply(mat1, mat2), mat3);
     std.mem.doNotOptimizeAway(&result);
 }
+
--- 02_math_libs_benchmark/src/benchmarks/quaternion.zig
+++ 02_math_libs_benchmark/src/benchmarks/quaternion.zig
@@ -6,22 +6,22 @@ const zm = @import("zm");
 pub fn bench_zalgebra_quat_mul(allocator: std.mem.Allocator) void {
     _ = allocator;
     const axis_vec = zalgebra.Vec3.new(1, 0, 0);
-    const quat_a = zalgebra.Quat.fromAxis(axis_vec, std.math.pi / 4.0);
+    const quat_a = zalgebra.Quat.fromAxis(std.math.pi / 4.0, axis_vec);
     const axis_vec2 = zalgebra.Vec3.new(0, 1, 0);
-    const quat_b = zalgebra.Quat.fromAxis(axis_vec2, std.math.pi / 6.0);
-    
+    const quat_b = zalgebra.Quat.fromAxis(std.math.pi / 6.0, axis_vec2);
+
     var result = zalgebra.Quat.mul(quat_a, quat_b);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_quat_mul(allocator: std.mem.Allocator) void {
     _ = allocator;
-    const axis_a = [_]f32{ 1, 0, 0, 0 };
-    const quat_a = zm.quatFromAxisAngle(axis_a, std.math.pi / 4.0);
-    const axis_b = [_]f32{ 0, 1, 0, 0 };
-    const quat_b = zm.quatFromAxisAngle(axis_b, std.math.pi / 6.0);
-    
-    var result = zm.qmul(quat_a, quat_b);
+    const axis_a = [_]f32{ 1, 0, 0 };
+    const quat_a = zm.Quaternionf.fromAxisAngle(axis_a, std.math.pi / 4.0);
+    const axis_b = [_]f32{ 0, 1, 0 };
+    const quat_b = zm.Quaternionf.fromAxisAngle(axis_b, std.math.pi / 6.0);
+
+    var result = zm.Quaternionf.multiply(quat_a, quat_b);
     std.mem.doNotOptimizeAway(&result);
 }
 
@@ -29,36 +29,36 @@ pub fn bench_zm_quat_mul(allocator: std.mem.Allocator) void {
 pub fn bench_zalgebra_quat_normalize(allocator: std.mem.Allocator) void {
     _ = allocator;
     const axis_vec = zalgebra.Vec3.new(1, 1, 1);
-    const quat = zalgebra.Quat.fromAxis(axis_vec, std.math.pi / 4.0);
-    
+    const quat = zalgebra.Quat.fromAxis(std.math.pi / 4.0, axis_vec);
+
     var result = zalgebra.Quat.norm(quat);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_quat_normalize(allocator: std.mem.Allocator) void {
     _ = allocator;
-    const axis = [_]f32{ 1, 1, 1, 0 };
-    const quat = zm.quatFromAxisAngle(axis, std.math.pi / 4.0);
-    
-    var result = zm.normalize(quat);
-    std.mem.doNotOptimizeAway(&result);
+    const axis = [_]f32{ 1, 1, 1 };
+    var quat = zm.Quaternionf.fromAxisAngle(axis, std.math.pi / 4.0);
+
+    zm.Quaternionf.normalize(&quat);
+    std.mem.doNotOptimizeAway(&quat);
 }
 
 // Basic quaternion operations
 pub fn bench_zalgebra_quat_basic(allocator: std.mem.Allocator) void {
     _ = allocator;
     const axis_vec = zalgebra.Vec3.new(0, 1, 0);
-    const quat = zalgebra.Quat.fromAxis(axis_vec, std.math.pi / 4.0);
-    
+    const quat = zalgebra.Quat.fromAxis(std.math.pi / 4.0, axis_vec);
+
     var result = zalgebra.Quat.norm(quat);
     std.mem.doNotOptimizeAway(&result);
 }
 
 pub fn bench_zm_quat_basic(allocator: std.mem.Allocator) void {
     _ = allocator;
-    const axis = [_]f32{ 0, 1, 0, 0 };
-    const quat = zm.quatFromAxisAngle(axis, std.math.pi / 4.0);
-    
-    var result = zm.normalize(quat);
-    std.mem.doNotOptimizeAway(&result);
+    const axis = [_]f32{ 0, 1, 0 };
+    var quat = zm.Quaternionf.fromAxisAngle(axis, std.math.pi / 4.0);
+
+    zm.Quaternionf.normalize(&quat);
+    std.mem.doNotOptimizeAway(&quat);
 }
